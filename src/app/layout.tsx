f7f0e4bef3771ac6e30cbd1af24a05de3293c0f3
munaray <saabdulsalam1215225@gmail.com>
2026-01-02 12:38:15 +0100

    implement the brand assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,37 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Process Zero",
   description: "Process Zero Landing Page",
+  icons: {
+    icon: [
+      {
+        url: "/brand-assets/favicons/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/brand-assets/favicons/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/brand-assets/favicons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/brand-assets/favicons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/brand-assets/favicons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
